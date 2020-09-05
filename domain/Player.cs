9b8ab44aee045e0b758663802bfb03963f7b61ba
Renato Martins <guillenmartins@gmail.com>
2020-09-05 13:19:25 +0100

    email field added

--- domain/Player.cs
+++ domain/Player.cs
@@ -2,5 +2,7 @@ namespace testGit.domain {
     public class Player {
         public string Username { get; set; }
         public double Rating { get; set; }
+
+        public string email { get; set; }
     }
 }
